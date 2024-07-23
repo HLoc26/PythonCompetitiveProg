ccf674d7a2eb35c8d0bce3dca012aa3e71162ab8
HLoc26 <dhl26052004@gmail.com>
2024-07-23 14:18:37 +0700

    Three Integer Sum

---
+++ Neetcode/Two Pointers/3IntSum.py
@@ -0,0 +1,32 @@
+'''https://neetcode.io/problems/three-integer-sum'''
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        res = []
+        i = 0
+        while i < len(nums) - 2 and nums[i] <= 0:
+            if i > 0 and nums[i] == nums[i-1]: # Skip duplicate first num
+                i+=1
+                continue
+            j = i+1
+            k = len(nums)-1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if total < 0:
+                    j+=1
+                elif total > 0:
+                    k-=1
+                else:
+                    found =[nums[i], nums[j], nums[k]]
+                    if found not in res:
+                        res.append(found)
+                    j+=1
+                    k-=1
+                    while j < k and nums[j] == nums[j-1]:
+                        j+=1
+            i+=1
+        return res
+
+sol = Solution()
+nums = [0,0,0]
+print(sol.threeSum(nums))
