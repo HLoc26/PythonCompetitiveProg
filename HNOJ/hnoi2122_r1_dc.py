abb5f9b415b8dd27c05b399cecaf47bb9531aa24
HLoc26 <dhl26052004@gmail.com>
2024-08-05 22:14:15 +0700

    Điểm chung

---
+++ HNOJ/hnoi2122_r1_dc.py
@@ -0,0 +1,24 @@
+'''https://hnoj.edu.vn/problem/hnoi2122_r1_dc'''
+# AC 8/10, WA 2/10
+import sys
+sys.stdin = open('DC.INP','r')
+sys.stdout = open('DC.OUT','w')
+
+n, k = map(int, input().split())
+d = {}
+for _ in range(n):
+    a, b = map(int, input().split())
+    d[a] = d.get(a, 0) + 1
+    d[b] = d.get(b, 0) - 1
+points = sorted(d.keys())
+curr = 0
+ans = 0
+# Check if d[i] == k
+for i in range(len(points) - 1):
+    curr += d[points[i]]
+    if curr == k:
+        ans += points[i+1] - points[i]
+        if k == n:
+            ans += 1
+
+print(ans)
