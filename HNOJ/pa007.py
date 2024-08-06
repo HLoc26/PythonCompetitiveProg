1f7e8bc05009956235fa06d644891d9d6bd47cbc
HLoc26 <dhl26052004@gmail.com>
2024-08-06 20:18:00 +0700

    PA007

---
+++ HNOJ/pa007.py
@@ -0,0 +1,3 @@
+'''https://hnoj.edu.vn/problem/pa007'''
+h = float(input())
+print(f"{(2*9.8*h)**0.5:.3f}")
---
+++ HNOJ/pa010.py
@@ -0,0 +1,6 @@
+'''https://hnoj.edu.vn/problem/pa010'''
+
+a = int(input())
+b = int(input())
+
+print(f"{-b/a:.3f}")
