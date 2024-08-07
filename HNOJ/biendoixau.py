339ad4dde71dc0f1352aab93c2a1c302dd8adafe
HLoc26 <dhl26052004@gmail.com>
2024-08-07 10:11:07 +0700

    Biến đổi xâu

--- HNOJ/biendoixau.py
+++ HNOJ/biendoixau.py
@@ -1,4 +1,37 @@
 '''https://hnoj.edu.vn/problem/biendoixau'''
+
+# Đọc input
+S = input().strip()
+m = int(input())
+a = list(map(int, input().split()))
+
+n = len(S)
+diff = [0] * (n + 1)
+
+# Xử lý từng lần đảo
+for ai in a:
+    left, right = ai - 1, n - ai
+    diff[left] ^= 1
+    diff[right + 1] ^= 1
+
+# Tính prefix xor
+prefix_xor = 0
+flip = [0] * n
+for i in range(n):
+    prefix_xor ^= diff[i]
+    flip[i] = prefix_xor
+
+# Xây dựng chuỗi kết quả
+result = [''] * n
+for i in range(n):
+    if flip[i]:
+        result[n-1-i] = S[i]
+    else:
+        result[i] = S[i]
+
+print(''.join(result))
+
+
 '''AC 2/25, TLE 23/25'''
 def find_odd_occurrences(arr):
     count = {}
