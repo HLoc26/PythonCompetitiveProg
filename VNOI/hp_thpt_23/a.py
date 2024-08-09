b5dec5545eea128a0684603695f522286a8f21ac
HLoc26 <dhl26052004@gmail.com>
2024-08-09 23:21:56 +0700

    HSG THPT Hải Phòng 2023 - Bài 1

---
+++ VNOI/hp_thpt_23/a.py
@@ -0,0 +1,24 @@
+def factor(n):
+    f = {}
+    i = 2
+    while i*i <= n:
+        if n % i:
+            i+=1
+        else:
+            n//=i
+            f[i] = f.get(i, 0) + 1
+    if n > 0:
+        f[n] = f.get(n, 0) + 1
+    return list(f.values())
+
+n = int(input())
+
+if n < 2:
+    print(1)
+else:
+
+    f = factor(n)
+    res = 1
+    for i in range(len(f)):
+        res *= f[i] + 1
+    print(res)
