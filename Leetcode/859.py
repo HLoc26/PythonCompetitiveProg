0b85782e7d44cdeca8a78a1a48c95bb3be51fc25
HLoc26 <dhl26052004@gmail.com>
2024-08-28 16:37:22 +0700

    Buddy Strings

---
+++ Leetcode/859.py
@@ -0,0 +1,21 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if s == goal:
+            return len(set(s)) < len(s)
+        s = list(goal)
+        goal = list(goal)
+        n = len(s)
+        i, j = 0, n-1
+        while i < n and s[i] == goal[i]:
+            i+=1
+        while j > 0 and s[j] == goal[j]:
+            j-=1
+
+        if i < j:
+            s[i], s[j] = s[j], s[i]
+        return s == goal
+
+sol = Solution()
+s = 'ab'
+goal = 'ba'
+print(sol.buddyStrings(s, goal))
