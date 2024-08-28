5f15486bdfc8f2d98b695b9f9c4d426b3ffef447
HLoc26 <dhl26052004@gmail.com>
2024-08-28 16:36:38 +0700

    Valid Anagram

---
+++ Leetcode/242.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = dict()
+        if len(s) != len(t):
+            return False
+        
+        # Decrement the frequency of characters in string t
+        for i in range(len(s)):
+            count[s[i]] = count.get(s[i], 0) + 1
+            count[t[i]] = count.get(t[i], 0) - 1
+        
+        # Check if any character has non-zero frequency
+        for val in count.values():
+            if val != 0:
+                return False
+        print(count)
+        
+        return True
+
+sol = Solution()
+s = 'anagram'
+t = 'nagaram'
+print(sol.isAnagram(s, t))
