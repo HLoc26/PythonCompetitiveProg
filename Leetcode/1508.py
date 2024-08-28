9e4605459f899943a3b0a2ea958ac6cc1bf2512f
HLoc26 <dhl26052004@gmail.com>
2024-08-28 17:05:59 +0700

    Range Sum of Sorted Subarray Sums

--- Leetcode/1508.py
+++ Leetcode/1508.py
@@ -1,10 +1,16 @@
-'''https://leetcode.com/problems/range-sum-of-sorted-subarray-sums/description/'''
+'''https://leetcode.com/problems/range-sum-of-sorted-subarray-sums/'''
 class Solution:
     def rangeSum(self, nums: list[int], n: int, left: int, right: int) -> int:
-        prefix = self.prefix
-    
-    def prefix(self, nums: list[int]):
-        pref = [nums[0]]
-        for i in range(1, len(nums)):
-            pref.append(pref[i-1] + nums[i])
-        return pref
+        subArr_sum = []
+
+        for i in range(n):
+            curSum = 0
+            for j in range(i, n):
+                curSum += nums[j]
+                subArr_sum.append(curSum)
+        
+        subArr_sum.sort()
+        mod = 10**9 + 7
+
+        res = sum(subArr_sum[left-1 : right]) % mod
+        return res
